3c9af4e087ff49f5838a641dd7c5a0a1cec97ce9
tentato <alepaw99@gmail.com>
2021-12-01 18:31:05 +0100

    mouse - operations

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import pyautogui
+import pyautogui as pt
 import pyperclip
 
 from pynput.mouse import Button, Controller
 from time import sleep
 
-pyautogui.FAILSAFE = True
+pt.FAILSAFE = True
 MOUSE = Controller()
+
+# Do obrazu
+def to_img(img_path, clicks, offset_x = 0, offset_y = 0):
+    pos = pt.locateCenterOnScreen(img_path, confidence = .8)
+
+    if pos is None:
+        print("Image {} not found...".format(img_path))
+        return 0
+    else:
+        pt.moveTo(pos, duration = 1)
+        pt.moveRel(offset_x, offset_y, duration = .5)
+        pt.click(clicks = clicks, interval = .1)
